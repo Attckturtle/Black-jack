0660ff081a246e1273b2993e551ed6d4510d304b
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-03-29 18:28:09 +0800

    Add files via upload

--- blackJack/blackJack/blackJack.cpp
+++ blackJack/blackJack/blackJack.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
 #include <vector>
-#include<algorithm>
+
 
 class accountMaker {
 public:
-	std::string username;
-	std::string password;
-	int currentBalance = 500;
+  std::string username;
+  std::string password;
+  int currentBalance = 500;
+  bool loggedIn = false;
 };
 
 accountMaker account1;
@@ -14,7 +16,7 @@ accountMaker account2;
 accountMaker account3;
 
 std::vector<std::string> listOfAccountUsernames = {""};
-std::vector<std::string> listOfAccountPasswords = { "" };
+std::vector<std::string> listOfAccountPasswords = {""};
 std::vector<int> listOfDepositedValues = {0};
 
 int amountOfAccounts = 1;
@@ -23,110 +25,141 @@ void setUp();
 void makeAccount();
 int getAccountNumber(std::string a);
 void sendValuesToObject(int i);
-void login();
-void beginBlackjack();
+void login(int theAccountNumber);
+void beginBlackjack(int a);
 
 void setUp() {
-	int a;
-	std::cout << "1. Login to an account\n";
-	std::cout << "2. Make an account\n";
-    std::cin >> a;
-	switch (a) {
-	case 1:
-		login();
-		break;
-	case 2:
-		makeAccount();
-		break;
-	default:
-		std::cout << "Pick a valid option\n";
-	}
+  int a;
+  std::cout << "1. Login to an account\n";
+  std::cout << "2. Make an account\n";
+  std::cin >> a;
+  switch (a) {
+  case 1:
+      int saveNumber;
+      std::cout << "Select save file number\n";
+      std::cin >> saveNumber;
+    login(saveNumber);
+    break;
+  case 2:
+    makeAccount();
+    break;
+  default:
+    std::cout << "Pick a valid option\n";
+  }
 }
 
 void makeAccount() {
-	int accountNumber;
-	std::cout << "Enter a username\n";
-	std::string userName;
-	std::cin >> userName;
+  int accountNumber;
+  std::cout << "Enter a username\n";
+  std::string userName;
+  std::cin >> userName;
 
-	std::cout << "Enter a password\n";
-	std::string password;
-	std::cin >> password;
+  std::cout << "Enter a password\n";
+  std::string password;
+  std::cin >> password;
 
-	listOfAccountUsernames.push_back(userName);
-	listOfAccountPasswords.push_back(password);
-	listOfDepositedValues.push_back(0);
+  listOfAccountUsernames.push_back(userName);
+  listOfAccountPasswords.push_back(password);
+  listOfDepositedValues.push_back(0);
 
-	accountNumber = getAccountNumber(userName);
-	sendValuesToObject(accountNumber);
-	std::cout << accountNumber;
-	std::cout << "New account created\n";
-	login();
+  accountNumber = getAccountNumber(userName);
+  sendValuesToObject(accountNumber);
+  std::cout << "New account created\n";
+  std::cout << "Using save" << accountNumber;
+  switch (accountNumber) {
+  case 1:
+    account1.loggedIn = true;
+    break;
+  case 2:
+    account2.loggedIn = true;
+    break;
+  case 3:
+    account3.loggedIn = true;
+  }
+  beginBlackjack(accountNumber);
 }
 
 int getAccountNumber(std::string a) {
-	for (int i = 0; i <= listOfAccountUsernames.size(); i++) {
-		if (listOfAccountUsernames[i] == a) {
-			return i;
-		}
-	}
+  for (int i = 0; i <= listOfAccountUsernames.size(); i++) {
+    if (listOfAccountUsernames[i] == a) {
+      return i;
+    }
+  }
 }
 
 void sendValuesToObject(int i) {
-	int a = i;
-	a--;
-	switch (i) {
-	case 1:
-		account1.username = listOfAccountUsernames[a];
-		account1.password = listOfAccountPasswords[a];
-		account1.currentBalance = listOfDepositedValues[a];
-		break;
-	case 2:
-		account2.username = listOfAccountUsernames[a];
-		account2.password = listOfAccountPasswords[a];
-		account2.currentBalance = listOfDepositedValues[a];
-		break;
-	case 3:
-		account3.username = listOfAccountUsernames[a];
-		account3.password = listOfAccountPasswords[a];
-		account3.currentBalance = listOfDepositedValues[a];
-		break;
-	}
+  int a = i;
+  a--;
+  switch (i) {
+  case 1:
+    account1.username = listOfAccountUsernames[a];
+    account1.password = listOfAccountPasswords[a];
+    account1.currentBalance = listOfDepositedValues[a];
+    break;
+  case 2:
+    account2.username = listOfAccountUsernames[a];
+    account2.password = listOfAccountPasswords[a];
+    account2.currentBalance = listOfDepositedValues[a];
+    break;
+  case 3:
+    account3.username = listOfAccountUsernames[a];
+    account3.password = listOfAccountPasswords[a];
+    account3.currentBalance = listOfDepositedValues[a];
+    break;
+  }
 }
 
-void login() {
-	std::string username;
-	std::string password;
-	int accountNumber;
-	for (int i = 0; i < 3; i++) {
-		switch (i) {
-		case 1:
-			if (account1.username == username) {
-				accountNumber = 1;
-			}
-			break;
-		}
-	}
-	if (std::binary_search(listOfAccountUsernames.begin(), listOfAccountUsernames.end(), username)) {
-		if (std::binary_search(listOfAccountPasswords.begin(), listOfAccountPasswords.end(), password)) {
-			switch (accountNumber) {
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			}
-		}
-	}
+void login(int theAccountNumber) {
+  std::string username;
+  std::string password;
+
+  int accountNumber = getAccountNumber(username);
+
+  switch (theAccountNumber) {
+  case 1:
+    if (listOfAccountUsernames[accountNumber] == username) {
+    }
+  }
+  /*if (std::binary_search(listOfAccountUsernames.begin(),
+  listOfAccountUsernames.end(), username)) { if
+  (std::binary_search(listOfAccountPasswords.begin(),
+  listOfAccountPasswords.end(), password)) { std::cout << "Logged in\n"; for
+  (int i = 0; i < 3; i++) { switch (i) { case 1: if (account1.username ==
+  username) { accountNumber = 1; std::cout << "Using save 1\n";
+                                  }
+                                  break;
+                          case 2:
+                                  if (account2.username == username) {
+                                          accountNumber = 2;
+                                          std::cout << "Using save 2\n";
+                                  }
+                                  break;
+                          case 3:
+                                  if (account3.username == username) {
+                                          accountNumber = 3;
+                                          std::cout << "Using save 3\n";
+                                  }
+                                  break;
+                          }
+                  }
+          }
+          else {
+                  std::cout << "Wrong username or password";
+          }
+  }
+  */
 }
 
-void beginBlackjack() {
-	std::cout << "You current balance is: " << account1.currentBalance;
-	std::cout << "How much do you want to bid\n";
+void beginBlackjack(int a) {
+  std::vector<bool> accountDeterminingVector;
+  accountDeterminingVector.push_back(account1.loggedIn);
+  accountDeterminingVector.push_back(account2.loggedIn);
+  accountDeterminingVector.push_back(account3.loggedIn);
+  std::cout << "You current balance is: " << account1.currentBalance;
+  std::cout << "How much do you want to bid\n";
 }
 
 int main() {
-	setUp();
-	return 0;
+  setUp();
+  return 0;
 }
